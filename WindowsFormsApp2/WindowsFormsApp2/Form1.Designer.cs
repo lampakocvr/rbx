00afd0e5fed044db8a008a6f61ce22ed06c3c50b
lampakocvr <89940011+lampakocvr@users.noreply.github.com>
2021-11-17 15:35:31 +0100

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class fckscxt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fckscxt));
+            this.Execute = new System.Windows.Forms.Button();
+            this.Attach = new System.Windows.Forms.Button();
+            this.Discord = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.robloxkill = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Execute
+            // 
+            this.Execute.BackColor = System.Drawing.Color.DimGray;
+            this.Execute.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Execute.ForeColor = System.Drawing.Color.White;
+            this.Execute.Location = new System.Drawing.Point(12, 329);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(102, 30);
+            this.Execute.TabIndex = 0;
+            this.Execute.Text = "Execute";
+            this.Execute.UseVisualStyleBackColor = false;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // Attach
+            // 
+            this.Attach.BackColor = System.Drawing.Color.DimGray;
+            this.Attach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Attach.ForeColor = System.Drawing.Color.White;
+            this.Attach.Location = new System.Drawing.Point(359, 329);
+            this.Attach.Name = "Attach";
+            this.Attach.Size = new System.Drawing.Size(101, 30);
+            this.Attach.TabIndex = 2;
+            this.Attach.Text = "Attach";
+            this.Attach.UseVisualStyleBackColor = false;
+            this.Attach.Click += new System.EventHandler(this.Attach_Click);
+            // 
+            // Discord
+            // 
+            this.Discord.BackColor = System.Drawing.Color.DimGray;
+            this.Discord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Discord.ForeColor = System.Drawing.Color.White;
+            this.Discord.Location = new System.Drawing.Point(573, 329);
+            this.Discord.Name = "Discord";
+            this.Discord.Size = new System.Drawing.Size(101, 30);
+            this.Discord.TabIndex = 3;
+            this.Discord.Text = "Discord";
+            this.Discord.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(23, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(495, 275);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // robloxkill
+            // 
+            this.robloxkill.BackColor = System.Drawing.Color.DimGray;
+            this.robloxkill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.robloxkill.ForeColor = System.Drawing.Color.White;
+            this.robloxkill.Location = new System.Drawing.Point(466, 329);
+            this.robloxkill.Name = "robloxkill";
+            this.robloxkill.Size = new System.Drawing.Size(101, 30);
+            this.robloxkill.TabIndex = 5;
+            this.robloxkill.Text = "Kill roblox";
+            this.robloxkill.UseVisualStyleBackColor = false;
+            this.robloxkill.Click += new System.EventHandler(this.robloxkill_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(685, 17);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDown_event);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove_Event);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_E);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(58)))), ((int)(((byte)(180)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(666, -5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 19);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "x";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Jailbreak auto rob";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(524, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(149, 275);
+            this.panel2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(3, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Comming soon";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(3, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Pet simulator X auto farm";
+            // 
+            // fckscxt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(685, 371);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.robloxkill);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Discord);
+            this.Controls.Add(this.Attach);
+            this.Controls.Add(this.Execute);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fckscxt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fckscxt Version: 1.4";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Button Attach;
+        private System.Windows.Forms.Button Discord;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button robloxkill;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyExploits;
+
+namespace WindowsFormsApp2
+{
+    public partial class fckscxt : Form
+    {
+        bool mouseDown;
+        private Point offset;
+
+        Module epic = new Module();
+        public fckscxt()
+        {
+            InitializeComponent();
+        }
+
+        private void Attach_Click(object sender, EventArgs e)
+        {
+            epic.LaunchExploit();
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            epic.ExecuteScript(richTextBox1.Text);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            epic.killRoblox();
+        }
+
+        private void robloxkill_Click(object sender, EventArgs e)
+        {
+            epic.killRoblox();
+        }
+
+        private void mouseDown_event(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDown = true;
+        }
+
+        private void MouseMove_Event(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+                Location = new Point(currentScreenPos.X - offset.X, currentScreenPos.Y - offset.Y);
+
+            }
+        }
+
+        private void MouseUp_E(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
